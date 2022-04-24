4f4497e83f6e6c0a3402861ede5e47f75532bdd2
Roger Kwong <rkwong12@gmail.com>
2022-04-24 18:53:06 -0400

    Initial commit of source code

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
