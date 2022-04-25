cd86995307a8e47cb7a483d3af3b64fe365041f9
Roger Kwong <rkwong12@gmail.com>
2022-04-25 19:35:14 -0400

    Add support for liking and disliking recipes

--- src/screens/recipe-details.js
+++ src/screens/recipe-details.js
@@ -1,57 +1,105 @@
 import { useState, React, useEffect } from "react";
 import Preview from "../utils/preview";
-import {Link, useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Axios from "axios";
 
 const RecipeDetails = () => {
-    const API_URL = "https://api.edamam.com/api/recipes/v2/{$}?type=public&app_id=67754742&app_key=93a49685d010e3cc69c2ee6b73df40ce"
-    const [recipeDetails, setRecipeDetails] = useState({});
-    const {recipeID} = useParams();
-    const RECIPE_URL = API_URL.replace("{$}", recipeID);
-    console.log(RECIPE_URL);
-    const getRecipeByID = async () => {
-        const response = await Axios.get(RECIPE_URL);
-        setRecipeDetails(response.data.recipe);
+  const API_URL =
+    "https://api.edamam.com/api/recipes/v2/{$}?type=public&app_id=67754742&app_key=93a49685d010e3cc69c2ee6b73df40ce";
+  const [recipeDetails, setRecipeDetails] = useState({});
+  const { recipeID } = useParams();
+  const RECIPE_URL = API_URL.replace("{$}", recipeID);
+  // TODO: Move to services
+  const getRecipeByID = async () => {
+    const response = await Axios.get(RECIPE_URL);
+    setRecipeDetails(response.data.recipe);
+  };
 
-    }
+  useEffect(() => {
+    getRecipeByID();
+  });
 
-
-
-
-    useEffect(() => { getRecipeByID() }, [])
-
-
-    return (
-            <div>
-                <h1>{recipeDetails.label}</h1>
-                <p> <img className="img-responsive" src={recipeDetails.image} height={180} className="me-2 float-start"/></p>
-                <ul> <li> Meal Type: {recipeDetails.mealType}</li>
-                    <li> Dish Type: {recipeDetails.dishType}</li>
-                    <li> Calories: {recipeDetails.calories}</li>
-                </ul>
-
-                <br/>
-                <button className="btn btn-primary btn-success mt-4 positive-relative rounded-pill">Like</button>
-                <button className="btn btn-primary btn-danger mt-4 position-relative rounded-pill">Dislike</button> <br/>
-            <br/>
-                <h3 className="mt-3"> Ingredients (for serving size: {recipeDetails.yield} ) </h3>
-                <div className="card " style={{"width": "18rem;"}}>
-                    <ul className="list-group list-group-flush">
-
-                    </ul>
-
-
-
-                </div>
-
-                <h3 className="mt-5">Leave a comment:</h3>
-                <textarea className="form-control w-50"/>
-                <button className="btn btn-primary mt-1">Post</button>
-                <ul className="list-group">TODO: Grab comments from database</ul>
-                <Preview json={recipeDetails}/>
-            </div>
-
-    )
+  // TODO: Move to services
+  const API_BASE = process.env.REACT_APP_API_BASE
+    ? process.env.REACT_APP_API_BASE
+    : "http://localhost:4000/api";
+  const handleLikes = async () => {
+    // Construct the recipe
+    // TODO: Add more?
+    const recipe = {
+      title: recipeDetails.label,
+      rid: recipeID,
+    };
+    console.log(recipe);
+    const response = await Axios.put(
+      API_BASE + "/recipes/like/" + recipeID,
+      recipe
+    );
+    return response.data;
+  };
+  const handleDislikes = async () => {
+    // Construct the recipe
+    // TODO: Add more?
+    const recipe = {
+      title: recipeDetails.label,
+      rid: recipeID,
+    };
+    const response = await Axios.put(
+      API_BASE + "/recipes/dislike/" + recipeID,
+      recipe
+    );
+    return response.data;
+  };
+  // TODO: Upload recipe to db when liking, add uuid or take hash as RID
+  return (
+    <div>
+      <h1>{recipeDetails.label}</h1>
+      <p>
+        <img
+          className="img-responsive me-2 float-start"
+          src={recipeDetails.image}
+          height={180}
+          alt="Recipe"
+        />
+      </p>
+      <ul>
+        <li> Meal Type: {recipeDetails.mealType}</li>
+        <li> Dish Type: {recipeDetails.dishType}</li>
+        <li> Calories: {recipeDetails.calories}</li>
+      </ul>
+      <br />
+      <button
+        className="btn btn-primary btn-success mt-4 positive-relative rounded-pill"
+        onClick={handleLikes}
+      >
+        Like
+      </button>
+      <button
+        className="btn btn-primary btn-danger mt-4 position-relative rounded-pill"
+        onClick={handleDislikes}
+      >
+        Dislike
+      </button>
+      <br />
+      <br />
+      <h3 className="mt-3">
+        Ingredients (for serving size: {recipeDetails.yield} )
+      </h3>
+      <div className="card " style={{ width: "18rem;" }}>
+        <ul className="list-group">
+          {recipeDetails.ingredients &&
+            recipeDetails.ingredients.map((recipe) => (
+              <li className="list-group-item">{recipe.text}</li>
+            ))}
+        </ul>
+      </div>
+      <h3 className="mt-5">Leave a comment:</h3>
+      <textarea className="form-control w-50" />
+      <button className="btn btn-primary mt-1">Post</button>
+      <ul className="list-group">TODO: Grab comments from database</ul>
+      <Preview json={recipeDetails} />
+    </div>
+  );
 };
 
 export default RecipeDetails;
--- src/screens/search-recipes.js
+++ src/screens/search-recipes.js
@@ -1,45 +1,56 @@
 import { useState, React, useRef, useEffect } from "react";
-import { Link, useNavigate, useParams, useLocation } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Axios from "axios";
-import Preview from "../utils/preview"
 
 const SearchRecipes = () => {
   const [recipes, setRecipes] = useState([]);
   const recipeNameRef = useRef();
-  const {searchString} = useParams();
+  const { searchString } = useParams();
   const navigate = useNavigate();
-  const loc = useLocation();
   const RECIPE_URL = "https://api.edamam.com/api/recipes/v2?type=public&q=";
-  const searchRecipeByName = async () => {
-    const recipeName = recipeNameRef.current.value
-    const response = await Axios.get(RECIPE_URL + recipeName + "&app_id=67754742&app_key=93a49685d010e3cc69c2ee6b73df40ce");
-    setRecipes(response.data.hits);
-    navigate(`/search/${recipeNameRef.current.value}`)
 
-  }
-  console.log(recipes);
+  // TODO: Move to services
+  const searchRecipeByName = async () => {
+    const recipeName = recipeNameRef.current.value;
+    const response = await Axios.get(
+      RECIPE_URL +
+        recipeName +
+        "&app_id=67754742&app_key=93a49685d010e3cc69c2ee6b73df40ce"
+    );
+    setRecipes(response.data.hits);
+    navigate(`/search/${recipeNameRef.current.value}`);
+  };
   useEffect(() => {
-      if (searchString) {
-          recipeNameRef.current.value = searchString
-          searchRecipeByName()
-      }
-  }, [])
+    if (searchString) {
+      recipeNameRef.current.value = searchString;
+      searchRecipeByName();
+    }
+  });
   return (
     <div>
       <h1>Search Recipes!</h1>
-      <button className="btn btn-primary float-end" onClick={searchRecipeByName}>Search</button>
+      <button
+        className="btn btn-primary float-end"
+        onClick={searchRecipeByName}
+      >
+        Search
+      </button>
       <input className="form-control w-75" ref={recipeNameRef} />
       <ul className="list-group mt-4">
-        {
-          recipes.map(recipe => 
-            <li className="list-group-item">
-              <Link to={`/details/${recipe.recipe.uri.substring(44)}`}>
-                <img className="me-4" src={recipe.recipe.image} height={120}/>
-                {recipe.recipe.label}
-              </Link>
-            </li>)
-        }
-        </ul>
+        {recipes.map((recipe) => (
+          <li className="list-group-item">
+            <Link to={`/details/${recipe.recipe.uri.substring(44)}`}>
+              <img
+                className="me-4"
+                src={recipe.recipe.image}
+                height={120}
+                alt="Food"
+              />
+              {recipe.recipe.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
